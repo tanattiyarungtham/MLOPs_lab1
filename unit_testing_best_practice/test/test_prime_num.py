76267d671f3a5fab9e73b9fdcdda2620c40e2795
Tanattiya <tanattiya.r@gmail.com>
2024-11-21 13:10:25 +0200

    Ignore compiled Python files and __pycache__

--- unit_testing_best_practice/test/test_prime_num.py
+++ unit_testing_best_practice/test/test_prime_num.py
@@ -5,7 +5,7 @@ sys.path += ['../src']
 from prime_num import *
 
 def test_is_prime():
-    assert is_prime(1) == True
+    assert is_prime(1) == False
     assert is_prime(3) == True
-    assert is_prime(4) == True
+    assert is_prime(4) == False
 
